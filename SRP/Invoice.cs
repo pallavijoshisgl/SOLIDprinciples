5ab1ca390a3bfeeb8499b8961774c6c3f9cddeb8
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-11-27 12:42:32 +0530

    Add code for Violating SRP

---
+++ SRP/Invoice.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    public class Invoice
+    {
+        public long InvoiceAmount { get; set; }
+        public DateTime InvoiceDate { get; set; }
+
+        public void AddInvoice()
+        {
+            //code to add invoice
+            //and then send mail
+            MailMessage mailMessage = new MailMessage("EmailFrom", "EmailTo", "EmailSubject", "EmailBody");
+            this.sendInvoiceMail(mailMessage);
+        }
+
+        public void sendInvoiceMail(MailMessage mailMessage)
+        {
+            try
+            {
+                //Here we need to write the code for Email Setting and sending the invoice email
+            }
+            catch(Exception ex)
+            {
+                //Error Logging
+                System.IO.File.WriteAllText(@"c:\ErrorLog.txt", ex.ToString());
+            }
+
+        }
+
+        public void deleteInvoice()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"c:\ErrorlogText.txt", ex.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SRPImplemented/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRPImplemented
+{
+
+    public interface ILogger
+    {
+        void Info(string message);
+        void Debug(string info);
+
+        void Error(string message, Exception ex);
+    }
+    public class Logger:ILogger
+    {
+       
+        void ILogger.Info(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILogger.Debug(string info)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILogger.Error(string message, Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MailSender
+    {
+        public string EmailFrom { get; set; }
+        public string EmailTo { get; set; }
+
+        public string EmailSubject { get; set; }
+
+        public string EmailBody { get; set; }
+
+        public void SendEmail()
+        {
+
+        }
+    }
+    public class Invoice
+    {
+
+        public long InvAmount { get; set; }
+        public DateTime InvDate { get; set; }
+
+        private ILogger fileLogger;
+
+        private MailSender mailSender;
+
+        public Invoice()
+        {
+            fileLogger = new Logger();
+            mailSender = new MailSender();
+        }
+
+        public void AddInvoice()
+        {
+            try
+            {
+                fileLogger.Info("Add method start");
+                mailSender.EmailFrom = "emailfrom@xyz.com";
+                mailSender.EmailTo = "emailto@xyz.com";
+                mailSender.EmailSubject = "Single Responsibility Principle";
+                mailSender.EmailBody = "A class should have only one reason to change";
+                mailSender.SendEmail();
+            }
+            catch (Exception ex)
+            {
+                fileLogger.Error("Error occurred while generating invoice ", ex);
+            }
+        }
+
+        public void DeleteInvoice()
+        {
+            try
+            {
+                fileLogger.Info("Delete Invoice start at @" + DateTime.Now);
+            }
+            catch(Exception ex)
+            {
+                fileLogger.Error("Error occured while deleting invoice", ex);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
