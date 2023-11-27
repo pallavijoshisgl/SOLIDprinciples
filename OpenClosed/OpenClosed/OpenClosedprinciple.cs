f45b55dc5f4d3aa32253702ddfdf02e55091ee9f
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-11-27 15:07:32 +0530

    Add code for Open Closed Violation

---
+++ OpenClosed/OpenClosed/OpenClosedprinciple.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosed
+{
+    public class OpenClosedprinciple
+    {
+        public double GetInvoiceDiscount(double amount,InvoiceType invoiceType)
+        {
+            double finalAmount = 0;
+
+            if(invoiceType == InvoiceType.FinalInvoice)
+            {
+                finalAmount = amount - 100;
+            }
+            else if(invoiceType == InvoiceType.ProposedInvoice)
+            {
+                finalAmount = amount - 50;
+            }
+            return finalAmount;
+        }
+
+        public enum InvoiceType
+        {
+            FinalInvoice,
+            ProposedInvoice
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
