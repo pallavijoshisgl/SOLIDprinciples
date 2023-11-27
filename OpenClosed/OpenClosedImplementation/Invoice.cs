f22825ba4713c082804bcf3c6f087610b70667d9
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-11-27 15:26:56 +0530

    Update code for Invoice class Open Closed

--- OpenClosed/OpenClosedImplementation/Invoice.cs
+++ OpenClosed/OpenClosedImplementation/Invoice.cs
@@ -38,4 +38,28 @@ namespace OpenClosedImplementation
             return base.GetInvoiceDiscount(amount) - 30;
         }
     }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Invoice amount : 10000");
+
+            Invoice FInvoice = new FinalInvoice();
+            double FInvoiceAmount = FInvoice.GetInvoiceDiscount(10000);
+            Console.WriteLine(FInvoiceAmount);
+
+
+            Invoice PInvoice = new ProposedInvoice();
+            double PInvoiceAmount = PInvoice.GetInvoiceDiscount(10000);
+            Console.WriteLine(PInvoiceAmount);
+
+            Invoice RInvoice = new RecurringInvoice();
+            double RinvoiceAmount = RInvoice.GetInvoiceDiscount(10000);
+            Console.WriteLine(RinvoiceAmount);
+
+            Console.ReadKey();
+
+        }
+    }
 }
